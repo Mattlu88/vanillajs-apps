e23b6bb93b26179ce3961f75acf243657532b01c
Matt Lu <matt.lu@civica.com.au>
2020-10-22 19:50:45 +1100

    Working on carousel

--- carousel/app.js
+++ carousel/app.js
@@ -22,41 +22,140 @@ const prevItem = (currIdx, arr) => {
   return arr[idx]
 }
 
-const showReview = (review) => {
-  const img = document.querySelector('.img')
-  img.setAttribute('src', review.img)
-
-  const name = document.querySelector('.name')
-  name.textContent = review.name
-
-  const title = document.querySelector('.title')
-  title.textContent = review.job
-
-  const text = document.querySelector('.text')
-  text.textContent = review.text
+const setActiveSlide = (slideElement) => {
+  slideElement.className = 'slide'
+  slideElement.classList.add('active')
 }
 
+const addSlideToContainer = (slideElement) => {
+  const slideConatiner = document.querySelector('.slide-container')
+  slideConatiner.appendChild(slideElement)
+}
+
+const setLastSlide = (slideElement) => {
+  slideElement.className = 'slide'
+  slideElement.classList.add('last')
+}
+
+const setNextSlide = (slideElement) => {
+  slideElement.className = 'slide'
+  slideElement.classList.add('next')
+}
+
+const createSlide = (item) => {
+  let article = document.createElement('article')
+  article.innerHTML = ` 
+    <img src=${item.img} class='img' alt=${item.name} />
+    <h4>${item.name}</h4>
+    <p>${item.job}</p>
+    <p>${item.text}</p>
+  `
+  return article
+}
 
 let currentIndex = 0
-showReview(people[currentIndex])
 
 const nextBtn = document.querySelector('.next-btn')
 const prevBtn = document.querySelector('.prev-btn')
 
-nextBtn.addEventListener("click", () => {
+const initSlide = () => {
+  currentIndex = 0
+  createLastSlide(people[people.length - 1])
+  const activeSlide = createSlide(people[0])
+  addSlideToContainer(activeSlide)
+  setActiveSlide(activeSlide)
+
   const item = nextItem(currentIndex, people)
   if (!item) {
     return
   }
-  showReview(item)
-  currentIndex = people.indexOf(item)
+  const nextSlide = createSlide(item)
+  setNextSlide(nextSlide)
+  addSlideToContainer(nextSlide)
+}
+
+const removeLastSlide = () => {
+  const slide = document.querySelector('.last')
+  if (!slide) {
+    return
+  }
+  slide.remove()
+}
+
+const removeNextSlide = () => {
+  const slide = document.querySelector('.next')
+  if (!slide) {
+    return
+  }
+  slide.remove()
+}
+
+const createNextSlide = (item) => {
+  const slide = createSlide(item)
+  setNextSlide(slide)
+  addSlideToContainer(slide)
+}
+
+const createLastSlide = (item) => {
+  const slide = createSlide(item)
+  setLastSlide(slide)
+  const slideConatiner = document.querySelector('.slide-container')
+  slideConatiner.prepend(slide)
+}
+
+const getNextSlide = () => {
+  const slide = document.querySelector('.next')
+  return slide
+}
+
+const setSlideActive = (slideElement) => {
+  slideElement.className = 'slide'
+  slideElement.classList.add('active')
+}
+
+nextBtn.addEventListener('click', () => {
+  if (currentIndex === people.length - 1) {
+    //return
+  }
+  removeLastSlide()
+  const activeSlide = document.querySelector('.active')
+  setLastSlide(activeSlide)
+
+  const slide = getNextSlide()
+  setSlideActive(slide)
+  currentIndex = currentIndex === people.length - 1 ? 0 : currentIndex + 1
+
+  const item = nextItem(currentIndex, people)
+  if (!item) {
+    return
+  }
+  createNextSlide(item)
+
 })
 
-prevBtn.addEventListener("click", () => {
+prevBtn.addEventListener('click', () => {
+  if (currentIndex === 0) {
+    // return
+  }
+
+  removeNextSlide()
+  const slide = document.querySelector('.active')
+  setNextSlide(slide)
+
+  const prevSlide = document.querySelector('.last')
+  if (!prevSlide) {
+    return
+  }
+  setSlideActive(prevSlide)
+  currentIndex = currentIndex === 0 ? people.length - 1 : currentIndex - 1
+
   const item = prevItem(currentIndex, people)
   if (!item) {
     return
   }
-  showReview(item)
-  currentIndex = people.indexOf(item)
+  createLastSlide(item)
 })
+
+window.onload = () => {
+  initSlide()
+}
