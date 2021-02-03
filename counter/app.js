2ffba9531739318c91a2e0d01f48e8351041e3d9
Matt Lu <Matt.Lu@civica.com.au>
2021-02-03 16:16:28 +1100

    make responsive

--- counter/app.js
+++ counter/app.js
@@ -6,9 +6,9 @@ let counter = {
   decrease: function () {
     this.value--;
   },
-  reset: function() {
+  reset: function () {
     this.value = 0;
-  }
+  },
 };
 
 let decreaseBtn = document.querySelector("#decrease");
@@ -16,27 +16,21 @@ let resetBtn = document.querySelector("#reset");
 let increaseBtn = document.querySelector("#increase");
 let span = document.querySelector("#value");
 
-window.onload = () => {
-    decreaseBtn.addEventListener("click", handleClickDecreaseBtn);
-    resetBtn.addEventListener("click", handleClickResetBtn);
-    increaseBtn.addEventListener("click", handleClickIncreaseBtn);
-}
-
 const handleClickDecreaseBtn = () => {
   console.log("decrease btn clicked");
   counter.decrease();
-  showCounterValue()
-}
+  showCounterValue();
+};
 
 const handleClickResetBtn = () => {
   counter.reset();
-  showCounterValue()
-}
+  showCounterValue();
+};
 
 const handleClickIncreaseBtn = () => {
   counter.increase();
-  showCounterValue()
-}
+  showCounterValue();
+};
 
 const showCounterValue = () => {
   span.innerHTML = counter.value;
@@ -49,4 +43,8 @@ const showCounterValue = () => {
   if (counter.value > 0) {
     span.style.color = "green";
   }
-}
+};
+
+decreaseBtn.addEventListener("click", handleClickDecreaseBtn);
+resetBtn.addEventListener("click", handleClickResetBtn);
+increaseBtn.addEventListener("click", handleClickIncreaseBtn);
